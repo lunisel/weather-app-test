15387f9d613ee9d81ec850dcc3ddd8d42ee72eb6
Luna Sarmiento <selene942008@hotmail.com>
2022-02-04 12:14:24 +0100

    format time logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,30 @@ function App() {
       const [lat, lon] = await fetchCity("Bologna");
 
       const weather = await fetchWeather(lat, lon);
-      console.log(weather);
+      const unixTimestamp = weather.current.dt;
+
+      const milliseconds = unixTimestamp * 1000; // 1575909015000
+
+      const dateObject = new Date(milliseconds);
+
+      const weekDay = dateObject.toLocaleString("en-US", { weekday: "long" });
+      const month = dateObject.toLocaleString("en-US", { month: "long" });
+      const day = dateObject.toLocaleString("en-US", { day: "numeric" });
+      const h = dateObject.toLocaleString("en-US", { hour: "numeric" });
+      const [hour, AmPm] = h.split(" ");
+      const minutes = dateObject.toLocaleString("en-US", { minute: "numeric" });
+
+      console.log(
+        weekDay,
+        day,
+        ",",
+        month,
+        ",",
+        hour.length === 2 ? hour : "0" + hour,
+        ":",
+        minutes.length === 2 ? minutes : "0" + minutes,
+        AmPm
+      );
     };
 
     fetch();
