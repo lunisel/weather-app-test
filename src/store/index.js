74b31bdd932ffb4d340d6d6d4ce2f0289a8e41ba
Luna Sarmiento <selene942008@hotmail.com>
2022-02-07 15:54:11 +0100

    redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,11 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import thunk from "redux-thunk";
 import mainReducer from "./reducers/index.js";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 export const initialState = {
   user: {
@@ -8,10 +14,24 @@ export const initialState = {
   },
 };
 
-const configureStore = createStore(
-  mainReducer,
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_ENCRYPT_KEY,
+    }),
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
+export const configureStore = createStore(
+  persistedReducer,
   initialState,
-  process.env.REACT_APP_DEVELOPMENT && window.__REDUX_DEVTOOLS_EXTENSION__()
+  process.env.REACT_APP_DEVELOPMENT
+    ? composeEnhancers(applyMiddleware(thunk))
+    : compose(applyMiddleware(thunk))
 );
 
-export default configureStore;
+export const persistor = persistStore(configureStore);
