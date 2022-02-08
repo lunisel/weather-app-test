d5f116bfdd6c518924e814d1006186dbebc95d80
Luna Sarmiento <selene942008@hotmail.com>
2022-02-08 17:18:45 +0100

    desktop started

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,7 +1,19 @@
+import HomeMobile from "./HomeMobile.jsx";
+import LocalizationMobile from "./LocalizationMobile.jsx";
+import SearchMobile from "./SearchMobile.jsx";
+
 const Desktop = () => {
   return (
-    <div className="big-container">
-      <h1>DESKTOP</h1>
+    <div className="big-container" id="desktop">
+      <div className="current-selected-city"></div>
+      <div className="current-other-cities">
+        <HomeMobile />
+      </div>
+      <div className="forcast-selected-city"></div>
+      <div className="search-localization">
+        <SearchMobile />
+        <LocalizationMobile />
+      </div>
     </div>
   );
 };
