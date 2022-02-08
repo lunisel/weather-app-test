ee1862ca6f2fa741b0ac7bb3e1286a91b69c0502
Luna Sarmiento <selene942008@hotmail.com>
2022-02-08 15:41:27 +0100

    detail page done

--- src/components/DetailMobile.jsx
+++ src/components/DetailMobile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { MdArrowBack } from "react-icons/md";
 import { BsPlusSquare, BsDashSquare } from "react-icons/bs";
-import { formatDate } from "./logic";
+import { formatDate, formatTime } from "./logic";
 import { useDispatch } from "react-redux";
 import { addCity, removeCity } from "../store/actions";
 
@@ -44,6 +44,43 @@ const Detail = ({ setPage }) => {
         <span className="temperature">
           {selectedCity.w.current.temp.toString().split(".")[0]}˚
         </span>
+      </div>
+      <div className="timeline-container">
+        <div className="now">
+          <p>Now</p>
+          <div className="circle"></div>
+          <span>{selectedCity.w.current.temp.toString().split(".")[0]}˚</span>
+        </div>
+        {selectedCity.w.hourly.map(
+          (h, i) =>
+            i < 5 && (
+              <div className="other-times">
+                <p>
+                  {parseInt(formatTime(h.dt).split(":")[0])}
+                  {formatTime(h.dt).split(" ")[1].toLowerCase()}
+                </p>
+                <div className="circle"></div>
+                <span>
+                  {selectedCity.w.hourly[i].temp.toString().split(".")[0]}˚
+                </span>
+              </div>
+            )
+        )}
+        <div className="line"></div>
+      </div>
+      <div className="next-days-container">
+        {selectedCity.w.daily.map(
+          (d, i) =>
+            i !== 0 && (
+              <div className="signle-day">
+                <h2>{formatDate(d.dt).split(" ")[0]}</h2>
+                <span>{d.temp.day.toString().split(".")[0]}˚</span>
+                <div className="icon-container">
+                  <img src={`/assets/${d.weather[0].icon}.png`} alt="icon" />
+                </div>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
--- src/components/HomeMobile.jsx
+++ src/components/HomeMobile.jsx
@@ -1,15 +1,32 @@
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { addUserName } from "../store/actions";
 import AddCity from "./AddCity";
 import SingleCityCard from "./SingleCityCard";
 
 const HomeMobile = ({ page, setPage }) => {
   const cities = useSelector((state) => state.user.cities);
+  const name = useSelector((state) => state.user.firstName);
+  const dispatch = useDispatch();
 
   return (
     <div className="container home">
       <div className="title-container">
         <h2>Good Morning!</h2>
-        <h2>Mario</h2>
+        {name === "" ? (
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="name"
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                dispatch(addUserName(e.currentTarget.value));
+              }
+            }}
+          />
+        ) : (
+          <h2>{name}</h2>
+        )}
       </div>
       <AddCity page={page} setPage={setPage} />
       <div className="cities-container">
