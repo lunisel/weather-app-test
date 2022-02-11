bdfdbd5971cf63be5eca935185dba26bd0fc6111
Luna Sarmiento <selene942008@hotmail.com>
2022-02-11 21:39:20 +0100

    next month almost done

--- src/components/WeekMonthForecast.jsx
+++ src/components/WeekMonthForecast.jsx
@@ -52,7 +52,48 @@ const WeekMonthForecast = () => {
           </div>
         </div>
       ) : (
-        <div className="forecast-container"></div>
+        <div className="forecast-container month">
+          <div className="next-month-info-container">
+            <div className="date-icon-container">
+              <h3>
+                {formatDate(selectedCity.w.daily[7].dt)
+                  .split(" ")[0]
+                  .substring(0, 3)}
+                ,{" "}
+                {
+                  formatDate(selectedCity.w.daily[7].dt)
+                    .split(" ")[1]
+                    .split(",")[0]
+                }{" "}
+                {formatDate(selectedCity.w.daily[7].dt)
+                  .split(" ")[2]
+                  .substring(0, 3)}
+              </h3>
+              <img
+                src={`/assets/${selectedCity.w.daily[7].weather[0].icon}.png`}
+                alt="weather-icon"
+              />
+            </div>
+            <div className="description-info-container">
+              <p>
+                {selectedCity.w.daily[7].temp.day.toString().split(".")[0]}˚
+              </p>
+              <span>{selectedCity.w.daily[7].weather[0].main}</span>
+              <span>
+                The high will be {selectedCity.w.daily[7].temp.max}˚C, the low
+                will be {selectedCity.w.daily[7].temp.min}˚C.
+              </span>
+              <div className="info">
+                <span>Humidity: {selectedCity.w.daily[7].humidity}%</span>
+                <span>UV: {selectedCity.w.daily[7].uvi}</span>
+                <span>
+                  Dew point:{" "}
+                  {selectedCity.w.daily[7].dew_point.toString().split(".")[0]}˚C
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
