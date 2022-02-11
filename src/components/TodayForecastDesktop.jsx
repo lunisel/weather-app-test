6068a0f3a1385adc7e9aea8a2ba68791725c3128
Luna Sarmiento <selene942008@hotmail.com>
2022-02-11 12:30:30 +0100

    today desktop started

--- src/components/TodayForecastDesktop.jsx
+++ src/components/TodayForecastDesktop.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { formatTime } from "./logic";
 
 const TodayForecastDesktop = () => {
   const selectedCity = useSelector((state) => state.user.selectedCity);
@@ -8,7 +9,38 @@ const TodayForecastDesktop = () => {
       {selectedCity && (
         <div
           className={`time-weather-container bg-${selectedCity.w.current.weather[0].icon}`}
-        ></div>
+        >
+          <p className="now-desktop">Now</p>
+          <div className="timeline-container-desktop">
+            <div className="now">
+              <span>
+                {selectedCity.w.current.temp.toString().split(".")[0]}˚
+              </span>
+              <div className="circle"></div>
+              <p></p>
+            </div>
+            {selectedCity.w.hourly.map(
+              (h, i) =>
+                i < 3 && (
+                  <div className="other-times">
+                    <span>
+                      {selectedCity.w.hourly[i].temp.toString().split(".")[0]}˚
+                    </span>
+                    <div className="circle"></div>
+                    <p>
+                      {parseInt(
+                        formatTime(h.dt, selectedCity.w.timezone).split(":")[0]
+                      )}
+                      {formatTime(h.dt, selectedCity.w.timezone)
+                        .split(" ")[1]
+                        .toLowerCase()}
+                    </p>
+                  </div>
+                )
+            )}
+            <div className="line-desktop"></div>
+          </div>
+        </div>
       )}
     </div>
   );
